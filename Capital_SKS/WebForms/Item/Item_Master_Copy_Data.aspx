05d155d45c873de972361dd1f4caa2a4aea4aede
capital-aymisoe <capital.aymisoe@gmail.com>
2022-04-28 17:33:41 +0630

    byami

---
+++ Capital_SKS/WebForms/Item/Item_Master_Copy_Data.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Item_Master_Copy_Data.aspx.cs" Inherits="Capital_SKS.WebForms.Item.Item_Master_Copy_Data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>複製コピー</title>
+    <link rel="stylesheet" href="../../Styles/base.css" />
+    <link rel="stylesheet" href="../../Styles/common.css" />
+   <%-- <link rel="stylesheet" href="../../Styles/manager-style.css" />--%>
+   <link rel="stylesheet" href="../../Styles/item.css" />
+</head>
+<body>
+    <div id="PopWrapper">
+
+<section runat="server">
+	<h1><asp:Label runat="server" ID="lblheader" /></h1>
+	
+	<div id="PopContents" class="pop4_Mcate">
+    <form id="form1" runat="server">
+
+        <div>
+            <h3>複製コピー</h3>
+            <br/>
+         <dl class="itemNo">
+			<dt style="font-weight:unset">商品番号</dt>
+            <dd><asp:TextBox ID="txtItemCode" runat="server" Width="150px"></asp:TextBox></dd>
+		</dl>
+            <br/>
+		<dl class="itemName">
+			<dt style="font-weight:unset">商品名</dt>
+			<dd><asp:TextBox ID="txtItem_Name" runat="server" TextMode="MultiLine" Width ="550px" MaxLength="255"></asp:TextBox></dd>
+		</dl>
+        </div>
+        <div class="btn">
+            <asp:Button runat="server" ID="btnCopy" Text="複製" Width="150px" style="display:inline;margin-left:73px;" OnClick="btnCopy_Click" />
+            <asp:Button runat="server" ID="btnCancel" Text="キャンセル" Width="150px" style="display:inline;margin-left:80px;" OnClick="btnCancel_Click" />
+        </div>
+    </form>
+        </div>
+</section>
+</div>
+</body>
+</html>
---
+++ Capital_SKS/WebForms/Item/Item_Master_Copy_Data.aspx.cs
@@ -0,0 +1,108 @@
+﻿using ORS_RCM;
+using ORS_RCM_BL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Capital_SKS.WebForms.Item
+{
+    public partial class Item_Master_Copy_Data : System.Web.UI.Page
+    {
+        public string Item_Code
+        {
+            get
+            {
+                if (Request.QueryString["Item_Code"] != null)
+                {
+                    return Request.QueryString["Item_Code"].ToString();
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+        public string Item_Name
+        {
+            get
+            {
+                if (Request.QueryString["Item_Name"] != null)
+                {
+                    return Request.QueryString["Item_Name"].ToString();
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+        Item_Master_BL imeBL = new Item_Master_BL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    txtItemCode.Text = Item_Code;
+                    txtItem_Name.Text = Item_Name;
+                }
+            }
+            catch (Exception ex)
+            {
+                Session["Exception"] = ex.ToString();
+                Response.Redirect("~/CustomErrorPage.aspx?");
+            }
+
+        }
+
+        protected void btnCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String itemcode = txtItemCode.Text;
+                if (itemcode != null)
+                {
+                    int ItemID = imeBL.SelectItemID(itemcode);
+                    if (ItemID == 0) {
+                        DataTable dt = new DataTable();
+                        dt.Columns.Add("Item_Code", typeof(string));
+                        dt.Columns.Add("Item_Name", typeof(string));
+                        DataRow dr = dt.NewRow();
+                        dr["Item_Code"] = txtItemCode.Text;
+                        dr["Item_Name"] = txtItem_Name.Text;
+                        dt.Rows.Add(dr);
+
+                        Session["Item_Code_Copy"] = dt;
+                        this.ClientScript.RegisterClientScriptBlock(this.GetType(), "Close", "window.opener.__doPostBack();window.close()", true);
+                    }
+                    else
+                    {
+                        GlobalUI.MessageBox("Item code is already exists!!");
+                    }
+                }
+           }
+            catch (Exception ex)
+            {
+                Session["Exception"] = ex.ToString();
+                Response.Redirect("~/CustomErrorPage.aspx?");
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.ClientScript.RegisterClientScriptBlock(this.GetType(), "Close", "window.opener.__doPostBack();window.close()", true);
+            }
+            catch (Exception ex)
+            {
+                Session["Exception"] = ex.ToString();
+                Response.Redirect("~/CustomErrorPage.aspx?");
+            }
+        }
+    }
+}
